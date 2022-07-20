11afe5fb39cbab6251924f9c045d847b9e256c2a
Joshua Bankole-Williams <joshua.bankole-williams@getfutureproof.co.uk>
2022-07-20 11:37:49 +0100

    created the Blog model in models/blog.js - added get all, findById and create methods; added db configuration to the dbconfig.js file

---
+++ server/models/blog.js
@@ -0,0 +1,49 @@
+const db = require('../dbconfig'
+)
+class Blog {
+    constructor(data){
+        this.id = data.id
+        this.title = data.title
+        this.name = data.name
+        this.body = data.body
+    }
+
+    static get all() {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const blogsData = await db.query(`SELECT * FROM blogs;`)
+                const blogs = blogsData.rows.map(blog => new Blog(blog))
+                resolve(blogs);
+            } catch (err) {
+                reject("Error retrieving Blogs")
+            }
+        })
+    }
+
+    static findById (id) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let blogData = await db.query(`SELECT * FROM blogs WHERE id = $1;`, [ id ]);
+                let blog = new Blog(blogData.rows[0]);
+                resolve(blog);
+            } catch (err) {
+                reject('Blog not found');
+            }
+        })
+    }
+
+    static create(title, name, body) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let blogData = await db.query(`INSERT INTO blogs (title, name, body) VALUES ($1, $2, $3) RETURNING *;`, [ title, name, body]);
+                let newBlog = new Blog(blogData.rows[0]);
+                resolve(newBlog);
+            } catch (err) {
+                reject('Error creating blog')
+            }
+        })
+    }
+
+}
+
+module.exports = Blog
