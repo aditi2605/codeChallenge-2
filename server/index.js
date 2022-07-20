4748cc1f991375b86741e61ec425d10ac897b348
Matthew Richardson <matt.rich2045@gmail.com>
2022-07-20 11:05:45 +0100

    Added file structure for ejs files

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
 const server = require('./server');
+
 const port = process.env.PORT || 3000;
+
 server.listen(port, () => console.log(`🚀 express has started on port ${port}`));
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const ejs = require('ejs')
 //require router
 
 app.use(cors())
 app.use(express.json())
+app.set('view engine','ejs')
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req,res) => {
     res.send('Hello World')
