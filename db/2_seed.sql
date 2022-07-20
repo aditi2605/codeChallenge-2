10decd7cabb4df1bceeffcf38848546ada548c1c
Matthew Richardson <matt.rich2045@gmail.com>
2022-07-20 10:29:58 +0100

    Added database schema and seed

---
+++ db/2_seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO blogs (title, name, body) VALUES
+('Gordon Ramsay', 'Disgruntled Chef', 'He came to my restaurant and told me I don''t know how to cook. Then he called me a f***ing donkey.'),
+('Aoashi', 'Anime fan', 'It gives me Haikyuu!! vibes. Football is cool.');
